3f042c5c86de805e7d2e1b69048387bea6e43eb1
Elio-yang <2925539854@qq.com>
2020-07-03 18:45:15 +0800

    change import

--- src/Chapter1_2/DateTest.java
+++ src/Chapter1_2/DateTest.java
@@ -1,11 +1,6 @@
 package Chapter1_2;
 
-import edu.princeton.cs.algs4.Queue;
-import edu.princeton.cs.algs4.Stack;
-import jdk.nashorn.internal.parser.Scanner;
 
-import java.time.LocalDate;
-import java.util.HashMap;
 
 /**
  * Created by Elio Yang on 2020/7/2.
@@ -14,10 +9,8 @@ public class DateTest {
     public static void main(String[] args) {
         Date date1 = new Date(4, 23, 2020);
         Date date2 = new Date(2, 12, 2020);
-        LocalDate date3 = LocalDate.of(2020, 4, 23);
         System.out.println(date1.toString());
         System.out.println(date2.toString());
-        System.out.println(date3.toString());
 
         System.out.println(date1.equals(date2));
     }
