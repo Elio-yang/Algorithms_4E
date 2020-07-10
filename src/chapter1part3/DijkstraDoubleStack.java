0f5bfdd03c45ea290c68a2f2d6ff18111f60f650
Elio-yang <2925539854@qq.com>
2020-07-10 20:01:27 +0800

    description

--- src/chapter1part3/DijkstraDoubleStack.java
+++ src/chapter1part3/DijkstraDoubleStack.java
@@ -9,7 +9,15 @@ import java.util.Stack;
  */
 public class DijkstraDoubleStack {
     /**
-     *
+     * the first stack to store operations: + - * / ( )
+     * <p>
+     * the second stack to store values: double type
+     * <p>
+     * omit ( and push other operations when there is a )
+     * <p>
+     * stack_ops pop one element and doing arithmetic with two value in stack_val,
+     * push the answer into the stack_val,in the end pop the last element in stack_val
+     * will get the answer.
      * @param args the command line argument
      */
     public static void main(String[] args) {
